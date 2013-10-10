42e620c20bcf7e7f7e3c042922430d4735824724
Leon Radley <leon@digiplant.se>
2013-10-10 15:39:27 +0200

    Example express app

---
+++ example/server.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var path = require('path');
+var fs = require('fs');
+var util = require('util');
+var express = require('express');
+
+var app = express();
+app.use(express.bodyParser());
+
+app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname);
+app.set('view engine', 'html');
+
+// Static files
+app.use('/public', express.static(path.join(__dirname, './../')));
+app.use('/uploads', express.static(path.join(__dirname, './uploads')));
+
+// Routing
+app.use(app.router);
+
+var uploadPath = path.resolve(__dirname + '/uploads/');
+
+app.get('/uploads', function (req, res) {
+  fs.readdir(uploadPath, function (err, files) {
+    if (err) {
+      res.json([]);
+    }
+    res.json(files);
+  })
+});
+
+app.post('/upload', function(req, res) {
+  var files = util.isArray(req.files.file) ? req.files.file : [req.files.file];
+
+  console.log(files);
+
+  files.forEach(function (file) {
+    fs.rename(file.path, path.resolve(uploadPath, file.name), function(err) {
+      if (err) throw err;
+      fs.unlink(file.path, function() {
+        if (err) throw err;
+      });
+    });
+  });
+
+  // Force response type to text/html otherwise IE will try to open the returned json response.
+  res.contentType('text/html');
+
+  // Really should return json when all files have been saved, but we are simplifying things a bit here.
+  res.json({files: files.map(function (file) { return file.name; }) });
+});
+
+// Fallback route for angular
+var index = function (req, res) {
+  res.render('index');
+}
+
+app.get('*', index);
+app.get('/', index);
+
+module.exports = app;
