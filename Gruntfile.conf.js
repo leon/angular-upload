3c7f875578aa805def5bed7a8b5d95546200d001
Leon Radley <leon@radley.se>
2014-01-21 20:35:33 +0100

    Update grunt build and dependencies

--- Gruntfile.conf.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  src: 'src/**/*.js',
-  unit: 'test/**/*.js',
-  e2e: 'e2e/**/*.js',
-  less: 'src/**/*.less',
-  dist: 'angular-upload.js',
-  minified: 'angular-upload.min.js'
-};
