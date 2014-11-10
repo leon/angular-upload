e33bf9e89221062d256e5a673a0397047880b20f
Leon Radley <leon@digiplant.se>
2014-11-10 08:01:44 +0100

    Reset input after extraction of files #26

--- src/formDataUpload.js
+++ src/formDataUpload.js
@@ -5,7 +5,7 @@ angular.module('lr.upload.formdata', [])
   // Convert all data properties to FormData,
   // if they are a jqLite element, extract the files from the input
   .factory('formDataTransform', function () {
-    return function formDataTransform(data) {
+    return function formDataTransform (data) {
       var formData = new FormData();
 
       // Extract file elements from within config.data
@@ -19,6 +19,8 @@ angular.module('lr.upload.formdata', [])
             angular.forEach(el.files, function (file) {
               files.push(file);
             });
+            // Reset input value so that we don't upload the same files next time
+            el.value = '';
           });
 
           // Do we have any files?
